efaa096994c203db9b3dfc3260f6951436eef2fb
Jyoti sharma <86827121+jyotiSharma099@users.noreply.github.com>
2024-04-28 15:30:47 +0530

    Update Docker_jenkins_install.sh

--- Docker_jenkins_install.sh
+++ Docker_jenkins_install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#Docker Installetion
+#Docker Installation
 sudo apt update 
 sudo apt  install docker.io -y
 
 #Jenkins Installetion 
 #for jenkins firstly you need java so 
 sudo apt update
-sudo apt install openjdk-11-jre -y
+sudo apt install fontconfig openjdk-17-jre
 
 #Jenkins
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
